976bf9eae5b48d2ca65c0287fe8493a144936f03
Yi Yang <yy4382@outlook.com>
2026-04-13 15:04:06 +0800

    Add lazygit plugin

---
+++ lua/custom/plugins/lazygit.lua
@@ -0,0 +1,21 @@
+-- Plugin for calling lazygit from within neovim
+
+---@module 'lazy'
+---@type LazySpec
+return {
+  'kdheepak/lazygit.nvim',
+  lazy = true,
+  cmd = {
+    'LazyGit',
+    'LazyGitConfig',
+    'LazyGitCurrentFile',
+    'LazyGitFilter',
+    'LazyGitFilterCurrentFile',
+  },
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+  },
+  keys = {
+    { '<leader>gg', '<cmd>LazyGit<cr>', desc = '[G]it: Lazy[G]it' },
+  },
+}
